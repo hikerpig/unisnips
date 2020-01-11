70485c6c30eea4aabe5e099ef6436bbc8ecd7930
hikerpig <hikerpigwinnie@gmail.com>
2020-01-12 00:12:59 +0800

    [coverage] ignore files in '/lib/'

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/output/', '/tspackage/'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/lib/'],
   globals: {
     skipBabel: true,
   },
